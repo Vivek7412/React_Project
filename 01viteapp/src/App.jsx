c7a826271d3dd8cc745dc677948f1665c40bb0af
Vivek7412 <vivek7061054537@gmail.com>
2024-03-03 01:56:14 +0530

    first1

--- 01viteapp/src/App.jsx
+++ 01viteapp/src/App.jsx
@@ -1,10 +1,16 @@
 import Chai from "./chai"
 
 function App() {
+  const username="chai or hum"
 
 
   return (
-    <Chai/>
+    <>
+     <Chai/>
+    <h1>Chai or tum | {username}</h1>
+    <p>test paper </p>
+    </>
+   
 
   )
 }
--- 01viteapp/src/main.jsx
+++ 01viteapp/src/main.jsx
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+const anotherElement=(
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser=" Hum or React"
+
+const reactElement= React.createElement(
+    'a',
+    {href:'https://flipkart.com', target:'_blank'},
+    'Click me to visit Flipkart',
+    anotherUser
+)
+ 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
+    
+     reactElement
+    
+   
 )
