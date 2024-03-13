cc3f179fdb85b8a11fada10f2f587e58f4a6663c
Vivek7412 <vivek7061054537@gmail.com>
2024-03-13 14:30:57 +0530

    Todo locat compleated

--- ToDoContextLocal/src/Context/TodoContext.js
+++ ToDoContextLocal/src/Context/TodoContext.js
@@ -1,24 +1,22 @@
 import {createContext, useContext} from "react"
 
 export const TodoContext = createContext({
-    todos:[
+    todos: [
         {
-            id:1,
-            todo:"Todo Msg",
-            compleated:false,
+            id: 1,
+            todo: "Todo msg",
+            completed: false,
         }
     ],
-    addTodo:(todo) => {},
-    updateTodo:(id, todo) => {},
-    deleteTodo:(id) => {},
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
     toggleComplete: (id) => {}
 })
 
 
 export const useTodo = () => {
     return useContext(TodoContext)
-
 }
-
 
 export const TodoProvider = TodoContext.Provider
